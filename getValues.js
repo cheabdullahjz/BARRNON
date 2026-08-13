5068e17292f25c79842a062e3b5d8c1b714df113
cheabdullahjz <114251720+cheabdullahjz@users.noreply.github.com>
2026-08-13 12:41:19 +0100

    Add files via upload

---
+++ getValues.js
@@ -0,0 +1,29 @@
+ function getValues() {
+            var rows = document.querySelectorAll("tr.package-row");
+            rows.forEach(function (currentRow) {
+
+                var numberUsed = Number(currentRow.querySelector('#numberUsed').value);
+                var price = Number(currentRow.querySelector('#price').value);
+                var inPackage = Number(currentRow.querySelector('#inPackage').value);
+                var revenue = 0;
+
+                document.querySelectorAll('numberUsed');
+
+                if (numberUsed == "") {
+                    if (isNaN(inPackage) || isNaN(price)) {
+                        return;
+                    }
+                    revenue = price * inPackage;
+                }
+                else {
+                    if (isNaN(numberUsed) || isNaN(price)) {
+                        return;
+                    }
+                    revenue = price * numberUsed;
+                }
+                var value = revenue * 5;
+                currentRow.querySelector("#revenue").innerHTML = revenue;
+                currentRow.querySelector("#value").innerHTML = value;
+            });
+
+        }
